2490d423719b2b2f33977723f16e3b40e189fdea
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-21 08:28:28 +0530

    ADDED: Welcome Message Added

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+public class AddressBookMain {
+    /*
+    Declaring AddressBook Main Class
+    Printing Welcome Message
+     */
+    public static void main(String[] args){
+        System.out.println("Welcome To The AddressBook Program");
+    }
+}
