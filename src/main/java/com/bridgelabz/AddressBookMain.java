8df54196224ab6ae99abdc0c48f28d2aeba86f1d
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-21 09:20:30 +0530

    ADDED: Created Contacts

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,10 +1,21 @@
 package com.bridgelabz;
+import java.util.ArrayList;
 public class AddressBookMain {
     /*
-    Declaring AddressBook Main Class
-    Printing Welcome Message
+     Declaring AddressBook Main Class
+     Printing Welcome Message
      */
     public static void main(String[] args){
         System.out.println("Welcome To The AddressBook Program");
+        ArrayList<Contacts> contactList = new ArrayList<>();
+        Contacts newContact = new Contacts();
+        newContact.setFirstName("Govardhan Reddy");
+        newContact.setLastName("Bajjuri");
+        newContact.setAddressCity("Kodad");
+        newContact.setState("Telangana");
+        newContact.setZip(508204);
+        newContact.setPhoneNumber(966610767);
+        newContact.setEmail("gopi1998@gmail.com");
     }
 }
+
---
+++ src/test/java/com/bridgelabz/AddressBookTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+import org.junit.jupiter.api.Test;
+
+public class AddressBookTest {
+    @Test
+    public void givenNamesChecking(){
+
+    }
+}
