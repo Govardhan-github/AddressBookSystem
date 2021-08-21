65ae4a555cb7a951bdff6cf9ad8c069106cecb88
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-21 09:50:54 +0530

    ADDED: Created Contacts

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,4 +1,16 @@
 package com.bridgelabz;
-
 public class AddressBook {
+    /*
+    Declared Add Contacts Method
+     */
+    public static void addContacts() {
+        Contacts newContact = new Contacts();
+        newContact.setFirstName("Govardhan Reddy");
+        newContact.setLastName("Bajjuri");
+        newContact.setAddressCity("Kodad");
+        newContact.setState("Telangana");
+        newContact.setZip(508204);
+        newContact.setPhoneNumber(966610767);
+        newContact.setEmail("gopi1998@gmail.com");
+    }
 }
